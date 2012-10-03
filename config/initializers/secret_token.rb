4f5e791c008e515f4a123367a94c6385a1813e3c
asonas <hzw1258@gmail.com>
2012-10-03 21:38:12 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urquell::Application.config.secret_token = '7f0599a735fe0c6c69e4a3e9f2b4180a1eca07f01c6a5a304dc8b50c9dc313209f6835f8504aea0302ab289ce9398de03fa7d355639a10413e72cf7705bda667'
